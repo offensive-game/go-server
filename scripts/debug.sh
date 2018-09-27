f1e70a540020558f6529494208238b6a9c4d9d19
Djordje Vukovic <djvukovic@gmail.com>
2018-09-27 19:36:53 +0200

    Run & debug

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,2 +1,10 @@
-#!/usr/bin/env sh
-dlv debug ./simple-http.go --headless --accept-multiclient --listen=:2345 --api-version=2 --log
+#!/usr/bin/env bash
+while true; do
+    echo BUILDING...
+#    go build -gcflags "all=-N -l" ./cmd/offensive/main.go
+#    dlv exec ./main --headless --listen=:2345 --api-version=2 --log &
+    dlv debug ./cmd/offensive/main.go --headless --accept-multiclient --listen=:2345 --api-version=2 --log
+    echo Waiting for changes
+    inotifywait -r -e modify -e move -e create -e delete ./internal ./cmd ./pkg
+    pkill -P $$
+done
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 while true; do
     echo BUILDING...
-    ps -A
     go build ./cmd/offensive/main.go
     ./main &
     echo Waiting for changes
